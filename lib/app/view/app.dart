639dfd111d58966c221c6428a3bcb6f08dbf28a8
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-01-31 21:48:21 +0100

    Created PuzzleView class.

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:puzzlegami/modules/modules.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PuzzleView(),
+    );
   }
 }
---
+++ lib/modules/modules.dart
@@ -0,0 +1,1 @@
+export 'puzzle/puzzle.dart';
